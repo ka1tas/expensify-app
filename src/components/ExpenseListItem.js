bdd10320368f5debb0cc1220f3b284dcfdbb1c28
Saikat Singha Mahapatra <saikat.singhamahapatra@cognizant.com>
2020-03-30 23:17:38 +0530

    changed expense item page

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import {Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
-export  const ExpenseListItem = ({dispatch , id, description , amount , createdDate })=>(
-    <div>
-        <h2><Link to={`/edit/${id}`}>{description}</Link></h2>
-        <p> Amount : {amount} - Created At : {createdDate}</p>
-
-    </div>
+const ExpenseListItem = ({ id, description, amount, createdAt }) => (
+  <div>
+    <Link to={`/edit/${id}`}>
+      <h3>{description}</h3>
+    </Link>
+    <p>
+      {numeral(amount / 100).format('$0,0.00')}
+      -
+      {moment(createdAt).format('MMMM Do, YYYY')}
+    </p>
+  </div>
 );
 
-
-
 export default ExpenseListItem;
-
--- src/test/components/ExpenseListItem.test.js
+++ src/test/components/ExpenseListItem.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import  {shallow} from 'enzyme';
-import { ExpenseListItem } from "../../components/ExpenseListItem";
-import expenses from "../fixtures/expenses";
+import { shallow } from 'enzyme';
+import expenses from '../fixtures/expenses';
+import ExpenseListItem from '../../components/ExpenseListItem';
 
-
-test('should render with expense ',()=>{
-    const wraper = shallow(<ExpenseListItem {...expenses[2]}/>)
-    expect(wraper).toMatchSnapshot();
-})
+test('should render ExpenseListItem correctly', () => {
+  const wrapper = shallow(<ExpenseListItem {...expenses[0]} />);
+  expect(wrapper).toMatchSnapshot();
+});
